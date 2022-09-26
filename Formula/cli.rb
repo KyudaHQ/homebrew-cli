52ab8d0bac4f0d013aa2a7f95bd506b4fde9d6fb
Lorenzo Casari <casari.lorenzo@gmail.com>
2022-09-26 18:01:19 +0200

    init

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,4 +1,4 @@
-class CLI < Formula
+class Cli < Formula
     desc "Kyuda CLI"
     homepage "https://github.com/KyudaHQ/cli"
     url "https://tl-cli.s3.eu-west-1.amazonaws.com/v0.0.6/kyuda-macos-x64.tar.gz"
